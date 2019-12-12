8dc620d242b2c009a268fe0c597cfbf158e99b4b
Ravalohery Harimamy <mamyravalohery@gmail.com>
2019-12-12 10:41:37 +0300

    commit for eleven million

--- read_eleven_million.py
+++ read_eleven_million.py
@@ -1,10 +1,21 @@
 import connect_pg
 import dealing_csv
+import time
 
+start_time = time.time()
 
 with open("../../Downloads/DW/DATAS EXPORT/Kontiki_FR_Enrich1_20191106/Kontiki_FR_Enrich1_20191106.csv", "r", encoding="utf-8") as file:
     all_row = file.readlines()
     file.close()
 
+list_md5 = []
+for row in all_row:
+    each_obj = row.strip().split(";")
+    list_md5.append(each_obj[0])
+
+list_set_md5 = list(set(list_md5))
 
 print(" Here is the number of row \'Kontiki_FR_Enrich1_20191106.csv\' --> ", len(all_row))
+print(" len de la liste list_md5 = ", len(list_md5), " -- len set_liste", len(list_set_md5))
+
+print("execution time : %s secondes ---" % (time.time() - start_time))
--- translation_dash_under.py
+++ translation_dash_under.py
@@ -1,11 +1,21 @@
 import unittest
 import test
 
-w = 1
-h = 2
-c = 4
-if c and h != '':
-    print("OK")
+# w = 1
+# h = 2
+# c = 4
+# if c and h != '':
+#     print("OK")
+#
+# if h and w != 4:
+#     print('Mlay André')
 
-if h and w != 4:
-    print('Mlay André')
+ext = ['.eu', '.cu', '.img']
+chaine = "test.eu"
+# for ex in ext:
+#     if ex in chaine:
+#         print("True")
+#     else:
+#         print("False")
+
+list_cmp_ext = [print("True") if ex in chaine else print("False") for ex in ext]
